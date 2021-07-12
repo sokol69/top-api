b579940d91b41c25887a72fbde61f260cdd4b2ca
dmitry.sokolov <dmitry.sokolov@jetrockets.ru>
2021-07-12 11:46:46 +0300

    Added configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypegooseModule } from 'nestjs-typegoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,10 +8,16 @@ import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
+import { getMongoConfig } from './configs/mongo.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    TypegooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getMongoConfig,
+    }),
     AuthModule,
     TopPageModule,
     ProductModule,

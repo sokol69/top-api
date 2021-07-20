5447ae636c8f15cc6607ec9baa30813b02a2142d
dmitry.sokolov <dmitry.sokolov@jetrockets.ru>
2021-07-20 10:24:00 +0300

    Added ValidationPipe for review

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -7,16 +7,18 @@ import {
   Body,
   HttpException,
   HttpStatus,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 
 import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewService } from './review.service';
 import { REVIEW_NOT_FOUND } from './review.constants';
-
 @Controller('review')
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
 
+  @UsePipes(new ValidationPipe())
   @Post('create')
   async create(@Body() dto: CreateReviewDto) {
     return this.reviewService.create(dto);

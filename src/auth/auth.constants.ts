7282c659881f779a468062c9a860ae9217d44db8
dmitry.sokolov <dmitry.sokolov@jetrockets.ru>
2021-07-21 11:25:18 +0300

    Added auth sevice

---
+++ src/auth/auth.constants.ts
@@ -0,0 +1,1 @@
+export const ALREADY_REGISTERED_ERROR = 'Такой юзер уже существует';
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,30 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 
+import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
+import { ALREADY_REGISTERED_ERROR } from './auth.constants';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @UsePipes(new ValidationPipe())
   @Post('register')
-  async register(@Body() dto: AuthDto) {}
+  async register(@Body() dto: AuthDto) {
+    const existingUser = await this.authService.findUser(dto.login);
+    if (existingUser) {
+      throw new BadRequestException(ALREADY_REGISTERED_ERROR);
+    }
+    return this.authService.createUser(dto);
+  }
 
   @HttpCode(200)
   @Post('login')

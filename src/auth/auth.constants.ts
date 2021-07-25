bfb3516961ded4cfbf3116bdfeb66e2163246fce
dmitry.sokolov <dmitry.sokolov@jetrockets.ru>
2021-07-25 08:02:30 +0300

    Added login

--- src/auth/auth.constants.ts
+++ src/auth/auth.constants.ts
@@ -1,1 +1,3 @@
-export const ALREADY_REGISTERED_ERROR = 'Такой юзер уже существует';
+export const ALREADY_REGISTERED_ERROR = 'Такой пользователь уже существует';
+export const USER_NOT_FOUND_ERROR = 'Пользователь не найден';
+export const WRONG_PASSWORD_ERROR = 'Неверный логин или пароль';
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypegooseModule } from 'nestjs-typegoose';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { AuthController } from './auth.controller';
 import { UserModel } from './user.model';
 import { AuthService } from './auth.service';
+import { getJwtConfig } from '../configs/jwt.config';
 
 @Module({
   controllers: [AuthController],
@@ -16,6 +19,11 @@ import { AuthService } from './auth.service';
         },
       },
     ]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getJwtConfig,
+    }),
   ],
   providers: [AuthService],
 })

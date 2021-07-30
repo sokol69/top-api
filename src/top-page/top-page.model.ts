b82795d50c4aaac0a4c5fe892456cf2fbea47611
dmitry.sokolov <dmitry.sokolov@jetrockets.ru>
2021-07-30 14:18:30 +0300

    Added text search

--- src/top-page/top-page.model.ts
+++ src/top-page/top-page.model.ts
@@ -1,4 +1,4 @@
-import { prop } from '@typegoose/typegoose';
+import { index, prop } from '@typegoose/typegoose';
 import { TimeStamps, Base } from '@typegoose/typegoose/lib/defaultClasses';
 
 export enum TopLevelCategory {
@@ -31,6 +31,7 @@ class TopPageAdvantage {
 }
 
 export interface TopPageModel extends Base {}
+@index({ '$**': 'text' })
 export class TopPageModel extends TimeStamps {
   @prop({ enum: TopLevelCategory })
   firstCategory: TopLevelCategory;
--- src/top-page/top-page.service.ts
+++ src/top-page/top-page.service.ts
@@ -27,6 +27,10 @@ export class TopPageService {
       .exec();
   }
 
+  async findByText(text: string) {
+    return this.topPageModel.find({ $text: { $search: text, $caseSensitive: false } }).exec();
+  }
+
   async deleteById(id: string) {
     return this.topPageModel.findByIdAndDelete(id).exec();
   }

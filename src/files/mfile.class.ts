32619c518a5e540f76caa080da470e6db551c282
dmitry.sokolov <dmitry.sokolov@jetrockets.ru>
2021-08-02 15:56:23 +0300

    Added convertation to webp

---
+++ src/files/mfile.class.ts
@@ -0,0 +1,11 @@
+import { Express } from 'express';
+
+export class MFile {
+  originalname: string;
+  buffer: Buffer;
+
+  constructor(file: Express.Multer.File | MFile) {
+    this.buffer = file.buffer;
+    this.originalname = file.originalname;
+  }
+}
